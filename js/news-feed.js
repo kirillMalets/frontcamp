788ceb86cf47412d4b20b260e74c39faf8500014
Malets, Kirill <Kirill.Malets@Kronos.com>
2016-12-08 14:07:49 +0300

    Added pattern

--- js/news-feed.js
+++ js/news-feed.js
@@ -2,19 +2,34 @@
 
 import 'babel-polyfill';
 import 'whatwg-fetch';
-import { render } from './templates/news-feed-tpl';
+import {render} from './templates/news-feed-tpl';
 import '../less/styles.less';
 
 let url = 'https://newsapi.org/v1/articles?source=bbc-news&apiKey=5a17c17619c4477cafd1dd15468104ff',
 	element = document.querySelector('#news-block');
 
-fetch(url)
-	.then(response => response.json())
-	.then(data => processData(data))
-	.catch(error =>	console.log(error));
+class newsFeedFactory {
+	constructor(url) {
+		this.url = url;
+	};
 
-function processData(data) {
-	for (let article of data.articles) {
-		element.insertAdjacentHTML('beforeEnd', render(article));
+	getFeedData() {
+		return new Promise((resolve, reject) => {
+			fetch(this.url)
+				.then(response => response.json())
+				.then(data => resolve(data))
+				.catch(error => console.log(error));
+		})
 	}
+} //factory
+
+function showNews() {
+	let dataFactory = new newsFeedFactory(url);
+
+	dataFactory.getFeedData().then(data => {
+		for (let article of data.articles) {
+			element.insertAdjacentHTML('beforeEnd', render(article));
+		}
+	})
 }
+showNews(); //facade
